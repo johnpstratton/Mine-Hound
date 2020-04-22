c762a566080a3422613f48b169031f68c8c7f1ab
johnpstratton <john.p.stratton6@gmail.com>
2020-04-22 11:38:07 -0700

    table render complete

---
+++ leaderboard.js
@@ -0,0 +1,38 @@
+
+function Winners(name, score) {
+  this.name = name;
+  this.score = score;
+  allWinners.push(this);
+};
+
+var allWinners = [];
+// console.log(this.allWinners);
+
+Winners.prototype.renderTable = function () {
+  var leaderboardTable = document.getElementById('leaderboard');
+  for(var i = 0; i < 1; i++) {
+  var newTRow = document.createElement('tr');
+  var newTd = document.createElement('td');
+  newTd.textContent = this.name;
+  newTRow.appendChild(newTd);
+  leaderboardTable.appendChild(newTRow);
+  }
+  for(var i = 0; i < 1; i++) {
+    var newTRow = document.createElement('tr');
+    var newTd = document.createElement('td');
+    newTd.textContent = this.score;
+    newTRow.appendChild(newTd);
+    leaderboardTable.appendChild(newTRow);
+    }
+};
+
+
+
+
+
+var topWinner = new Winners('jimbob', 1);
+topWinner.renderTable();
+var midWinner = new Winners('Tim', 8);
+midWinner.renderTable();
+var lowWinner = new Winners('Loser', 324);
+lowWinner.renderTable();
